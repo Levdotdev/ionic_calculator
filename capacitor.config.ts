b18da623a0c249041a48c6727b666e0096769ff8
Levdotdev <brito.lancekianne@minsu.edu.ph>
2026-09-11 20:15:06 +0800

    final

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.example.photogallery',
-  appName: 'photo-gallery',
+  appId: 'io.example.calculator',
+  appName: 'Ionic Calculator',
   webDir: 'dist'
 };
 
